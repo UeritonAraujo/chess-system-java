2742eb4149c99f14f674e82d60315ba038cb09c4
UeritonAraujo <ueritonaraujo160@gmail.com>
2024-08-14 17:03:10 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Board.java
@@ -0,0 +1,36 @@
+package boardgame;
+
+public class Board {					// Tabuleiro
+	
+	private int rows;					// linhas
+	private int columns;				// colunas
+	private Piece [][] pieces;			// peças
+	
+	public Board() {
+	}
+
+	public Board(int rows, int columns) {
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];		// minha matriz de peças será instanciadas de acordo com as linhas e colunas
+	}
+
+	public int getRows() {
+		return rows;
+	}
+
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+
+	public int getColumns() {
+		return columns;
+	}
+
+	public void setColumns(int columns) {
+		this.columns = columns;
+	}
+	
+	
+
+}
